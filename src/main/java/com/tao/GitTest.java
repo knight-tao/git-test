17f4a3f92b9d3a34825345e2e1f4e426c22c0ace
tao <1950843553@qq.com>
2022-01-08 16:46:08 +0800

    first commit

---
+++ src/main/java/com/tao/GitTest.java
@@ -0,0 +1,11 @@
+package com.tao;
+
+/**
+ * @author tao
+ * @create 2022-01-08-16:42
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
